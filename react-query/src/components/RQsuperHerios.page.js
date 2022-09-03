8966826c6421c53b980c974ea9551a1495db5b86
alaaalh <alaaalh437@gmail.com>
2022-09-03 20:02:35 +0200

    fetching data, loading and handle error message by react query

--- react-query/src/components/RQsuperHerios.page.js
+++ react-query/src/components/RQsuperHerios.page.js
@@ -4,20 +4,33 @@ import { useState, useEffect } from "react";
 export default function RQsuperHeriosPage() {
   const [isloading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:4000/heros").then((res) => {
-      setData(res.data);
-      setLoading(false);
-    });
+    axios
+      .get("http://localhost:4000/heros")
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
   }, []);
 
   if (isloading) {
     return <div>isLoading...</div>;
   }
+
+  if (error) {
+    return <h2>{error}</h2>;
+  }
   return (
-    <>{data.map((hero) => {
-        return <h2 key={hero.id}>{hero.name}</h2>
-    })}</>
-  )
+    <>
+      {data.map((hero) => {
+        return <h2 key={hero.id}>{hero.name}</h2>;
+      })}
+    </>
+  );
 }
--- react-query/src/components/SuperHerios.page.js
+++ react-query/src/components/SuperHerios.page.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchData = () => {
+  return axios.get("http://localhost:4000/heros");
+};
 
 export default function SuperHeriosPage() {
+  const onSuccess = () => {
+    console.log("the fetch data is success");
+  };
+
+  const onFaild = () => {
+    console.log("the fetch data is faild");
+  };
+  
+  const { isLoading, data, isError, error, isFetched, refetch } = useQuery(
+    "super-hero",
+    fetchData,
+    {
+      onSuccess,
+      onFaild,
+    }
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError){
+    return <div>{error.message}</div>
+  }
+
   return (
-    <div>SuperHeriosPage</div>
-  )
+    <>
+      {data?.data.map((hero) => {
+        return <h2 key={hero.id}>{hero.name}</h2>;
+      })}
+    </>
+  );
 }
